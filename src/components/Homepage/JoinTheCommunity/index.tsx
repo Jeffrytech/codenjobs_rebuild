de8e308650920a1166819f2d2ee5e982f94db727
RehkMansa <godspowernathaniel25@gmail.com>
2022-11-19 16:50:47 +0100

    finished join the community section

--- src/components/Homepage/JoinTheCommunity/index.tsx
+++ src/components/Homepage/JoinTheCommunity/index.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 import ExternalLink from "../../ExternalLink";
 import { COMPANY_ENGLISH_DOCS_WEBSITE } from "../../../config/environment";
 import { darkBlue, white } from "../../../design/colors";
-import { JoinTheCommunityDocsButton, JoinTheCommunityRegisterButton, JoinTheCommunitySection, JoinTheCommunityTitle } from "./JoinTheCommunityCSS";
+import {
+  JoinTheCommunityDocsButton,
+  JoinTheCommunityRegisterButton,
+  JoinTheCommunitySection,
+  JoinTheCommunityTitle,
+} from "./JoinTheCommunityCSS";
 
 const JoinTheCommunity = () => {
   return (
-    <JoinTheCommunitySection
-      id="join-the-community"
-    >
+    <JoinTheCommunitySection id="join-the-community">
       <div>
-        <JoinTheCommunityTitle
-        >
-          Join the community.
-        </JoinTheCommunityTitle>
+        <JoinTheCommunityTitle>Join the community.</JoinTheCommunityTitle>
         <div
           style={{
             display: "flex",
@@ -21,30 +21,20 @@ const JoinTheCommunity = () => {
             justifyContent: "center",
           }}
         >
-          <Link
-            href="/register"
-          >
-            <JoinTheCommunityRegisterButton>
+          <Link href="/register">
+            <JoinTheCommunityRegisterButton className="flex items-center">
               Get Started
             </JoinTheCommunityRegisterButton>
           </Link>
-          <ExternalLink
-            href={COMPANY_ENGLISH_DOCS_WEBSITE}
-          >
-            <JoinTheCommunityDocsButton>
-              <span
-                style={{
-                  marginRight: "0.5rem",
-                }}
-              >
-                Learn More
-              </span>
+          <ExternalLink href={COMPANY_ENGLISH_DOCS_WEBSITE}>
+            <JoinTheCommunityDocsButton className="flex items-center justify-center gap-2">
+              Learn More
               <img
                 src="/static/design/link_white.svg"
+                className="h-[10px] mt-[2px]"
               />
             </JoinTheCommunityDocsButton>
           </ExternalLink>
-          
         </div>
       </div>
     </JoinTheCommunitySection>
